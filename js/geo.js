fef39596196860be1ffa970937b00eb9cb4f4226
Steven G Hall <stevenhall@Stevens-MacBook-Pro.local>
2015-04-11 22:27:18 -0700

    add geodecoder

--- js/geo.js
+++ js/geo.js
@@ -1,3 +1,50 @@
+export var geodecoder = function (features) {
+
+  var store = {}
+
+  for (var i = 0; i < features.length; i++) {
+    store[features[i].id] = features[i];
+  }
+
+  return {
+    find: function (id) {
+      return store[id];
+    },
+    search: function (lat, lng) {
+
+      var match = false;
+
+      var country, coords;
+
+      for (var i = 0; i < features.length; i++) {
+        country = features[i];
+        if(country.geometry.type === 'Polygon') {
+          match = pointInPolygon(country.geometry.coordinates[0], [lng, lat]);
+          if (match) {
+            return { 
+              code: features[i].id, 
+              name: features[i].properties.name
+            }
+          }
+        } else if (country.geometry.type === 'MultiPolygon') {
+          coords = country.geometry.coordinates;
+          for (var j = 0; j < coords.length; j++) {
+            match = pointInPolygon(coords[j][0], [lng, lat]);
+            if (match) {
+              return { 
+                code: features[i].id, 
+                name: features[i].properties.name
+              }
+            }
+          }
+        }
+      }
+
+      return null;
+    }
+  };
+};
+
 // http://www.ecse.rpi.edu/Homepages/wrf/Research/Short_Notes/pnpoly.html
 var pointInPolygon = function(poly, point) {
     
@@ -18,3 +65,5 @@ var pointInPolygon = function(poly, point) {
 
   return inside;
 };
+
+
