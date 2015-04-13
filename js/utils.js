8ea87eaa240cc8e9dd88cfa8d8b60ab284531142
Steven Hall <hall.steven.g@gmail.com>
2015-04-12 22:18:13 -0700

    add debounce to utils and refactor polygon search

--- js/utils.js
+++ js/utils.js
@@ -16,3 +16,29 @@ export function memoize(fn) {
     fn.memoize[key] = fn.apply(this, args);
   };
 }
+
+export function debounce(func, wait, immediate) {
+  var timeout;
+  return function() {
+    var context = this, args = arguments;
+    var later = function() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+};
+
+export var getTween = function (prop, to, time) {
+  time = time || 500;
+  var node = this;
+  var from = node[prop];
+  var interpol = d3.interpolateObject(from, to);
+  return function (t) {
+    node[prop].copy(interpol(t / time));
+    if (t >= time) return true;
+  };
+};
